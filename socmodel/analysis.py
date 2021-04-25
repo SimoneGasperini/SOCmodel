f5c64d3dd22cb5c5a71a3bf8ab3f0bb83c1ceac1
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-04-25 19:23:47 +0200

    upload final images

--- socmodel/analysis.py
+++ socmodel/analysis.py
@@ -8,16 +8,16 @@ from scipy.optimize import curve_fit
 from socmodel.source.state import RandomState
 from socmodel.source.connectivity import RandomConnectivity
 from socmodel.source.network import Network
-
+from socmodel.plotting import adjust_plot
 
 
 def plot_degree_distribution (savefig=False):
 
-  socmodel = Network(n=1000, alpha=0.2, beta=10., tau=10)
+  socmodel = Network(n=2000, alpha=0.2, beta=10., tau=10)
   print(socmodel, flush=True)
   socmodel.run(evolution_steps=20000)
 
-  Kin = np.sum(np.abs(socmodel.C), axis=0)
+  Kin = np.sum(np.abs(socmodel.C.toarray()), axis=0)
   mean_Kin = round(np.mean(Kin), 3)
   bins = np.arange(Kin.min() - 0.5, Kin.max() + 1.5)
 
@@ -25,11 +25,9 @@ def plot_degree_distribution (savefig=False):
 
   entries, bin_edges, _ = ax.hist(Kin, bins=bins, color='black', density=True, histtype='bar', rwidth=0.7)
   ax.set_xticks(bins + 0.5)
-  ax.set_xlabel('In-degree $K$', fontsize=14)
-  ax.set_ylabel('Rel. frequency', fontsize=14)
-  ax.text(x=0.7, y=0.9, s=f'$<K> \simeq$ {mean_Kin}', fontsize=14, transform=ax.transAxes)
-  ax.set_axisbelow(True)
-  ax.grid(linestyle='dotted')
+  ax.set_xlabel('In-degree $K$', fontsize=16)
+  ax.set_ylabel('Rel. frequency', fontsize=16)
+  ax.text(x=0.7, y=0.9, s=fr'$\langle K \rangle \simeq$ {mean_Kin}', fontsize=16, transform=ax.transAxes)
 
   poisson = lambda k, lamb : (lamb**k / factorial(k)) * np.exp(-lamb)
   bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
@@ -37,6 +35,7 @@ def plot_degree_distribution (savefig=False):
 
   x = np.linspace(start=bin_middles[0], stop=bin_middles[-1], num=100)
   ax.plot(x, poisson(x, *params), color='red')
+  adjust_plot(ax=ax)
 
   plt.show()
 
@@ -44,35 +43,35 @@ def plot_degree_distribution (savefig=False):
     fig.savefig('./images/K_distribution.pdf', bbox_inches='tight', dpi=1200)
 
 
-
 def plot_degree_convergence (savefig=False):
 
   connectivity = [{'prob':0., 'col':'tab:blue'},
-                  {'prob':0.003, 'col':'tab:orange'},
-                  {'prob':0.006, 'col':'tab:green'}]
+                  {'prob':0.002, 'col':'tab:orange'},
+                  {'prob':0.004, 'col':'tab:green'}]
 
   fig1, ax1 = plt.subplots(figsize=(8,6))
   fig2, ax2 = plt.subplots(figsize=(8,6))
 
   for c in connectivity:
 
-    socmodel = Network(n=600, alpha=0.2, beta=10., tau=10,
+    socmodel = Network(n=1000, alpha=0.2, beta=10., tau=10,
                        C_init=RandomConnectivity(pPlus=c['prob'], pMinus=c['prob']))
     print(socmodel, flush=True)
-    Kplus, Kminus, _ = socmodel.run(evolution_steps=20000)
+    Kplus, Kminus, _ = socmodel.run(evolution_steps=30000)
 
-    ax1.plot(Kplus, color=c['col'])
-    ax2.plot(Kminus, color=c['col'])
+    k = c['prob'] * socmodel.n
+    ax1.plot(Kplus, color=c['col'], label=r'$\langle K_{+} \rangle ^{ini} \simeq$ ' + str(k))
+    ax2.plot(Kminus, color=c['col'], label=r'$\langle K_{-} \rangle ^{ini} \simeq$ ' + str(k))
 
-  ax1.set_xlabel('Evolution steps', fontsize=14)
-  ax1.set_ylabel('In-degree $<K_{+}$>', fontsize=14)
-  ax1.set_axisbelow(True)
-  ax1.grid(linestyle='dotted')
+  ax1.set_xlabel('Evolution steps', fontsize=16)
+  ax1.set_ylabel(r'In-degree $\langle K_{+} \rangle$', fontsize=16)
+  ax1.legend(fontsize=16)
+  adjust_plot(ax=ax1)
 
-  ax2.set_xlabel('Evolution steps', fontsize=14)
-  ax2.set_ylabel('In-degree $<K_{-}$>', fontsize=14)
-  ax2.set_axisbelow(True)
-  ax2.grid(linestyle='dotted')
+  ax2.set_xlabel('Evolution steps', fontsize=16)
+  ax2.set_ylabel(r'In-degree $\langle K_{-} \rangle$', fontsize=16)
+  ax2.legend(fontsize=16)
+  adjust_plot(ax=ax2)
 
   plt.show()
 
@@ -81,10 +80,9 @@ def plot_degree_convergence (savefig=False):
     fig2.savefig('./images/Kminus_convergence.pdf', bbox_inches='tight', dpi=1200)
 
 
-
 def plot_degree_vs_beta (savefig=False):
 
-  betas = np.linspace(start=0., stop=20., num=100)
+  betas = np.linspace(start=0., stop=20., num=60)
   mean_Kplus = np.empty_like(betas)
   std_Kplus = np.empty_like(betas)
   mean_Kminus = np.empty_like(betas)
@@ -106,17 +104,15 @@ def plot_degree_vs_beta (savefig=False):
 
   ax1.errorbar(x=betas, y=mean_Kplus, yerr=std_Kplus, color='tab:blue',
                fmt='.', elinewidth=1, ecolor='black', capsize=2)
-  ax1.set_xlabel('Inverse temp. $\\beta$', fontsize=14)
-  ax1.set_ylabel('Avg. in-degree $<K_{+}$>', fontsize=14)
-  ax1.set_axisbelow(True)
-  ax1.grid(linestyle='dotted')
+  ax1.set_xlabel('Inverse temp. $\\beta$', fontsize=16)
+  ax1.set_ylabel(r'Avg. in-degree $\langle K_{+} \rangle$', fontsize=16)
+  adjust_plot(ax=ax1)
 
   ax2.errorbar(x=betas, y=mean_Kminus, yerr=std_Kminus, color='tab:orange',
                fmt='.', elinewidth=1, ecolor='black', capsize=2)
-  ax2.set_xlabel('Inverse temp. $\\beta$', fontsize=14)
-  ax2.set_ylabel('Avg. in-degree $<K_{-}$>', fontsize=14)
-  ax2.set_axisbelow(True)
-  ax2.grid(linestyle='dotted')
+  ax2.set_xlabel('Inverse temp. $\\beta$', fontsize=16)
+  ax2.set_ylabel(r'Avg. in-degree $\langle K_{-} \rangle$', fontsize=16)
+  adjust_plot(ax=ax2)
 
   plt.show()
 
--- socmodel/plotting.py
+++ socmodel/plotting.py
@@ -2,18 +2,34 @@ import numpy as np
 import pylab as plt
 
 
-def show_simulation (Kplus, Kminus, branchPar):
+def show_simulation (Kplus, Kminus, branchPar, savefig=False):
 
-  fig, ax = plt.subplots(figsize=(14,10))
+  fig, ax = plt.subplots(figsize=(8,6))
 
   x = np.arange(Kplus.size)
-  ax.plot(x, Kplus, label='In-degree $<K_{+}>$')
-  ax.plot(x, Kminus, label='In-degree $<K_{-}>$')
-  ax.plot(x[3000:], branchPar[3000:], label='Branching par $<\lambda>$')
+  ax.plot(x, Kplus, label=r'In-degree $\langle K_{+} \rangle$')
+  ax.plot(x, Kminus, label=r'In-degree $\langle K_{-} \rangle$')
+  ax.plot(x[4000:], branchPar[4000:], label=r'Branching par $\langle \lambda \rangle$')
 
-  ax.set_xlabel('Evolution steps', fontsize=14)
-  ax.legend(fontsize=12)
-  ax.set_axisbelow(True)
-  ax.grid(linestyle='dotted')
+  ax.set_xlabel('Evolution steps', fontsize=16)
+  ax.legend(fontsize=16)
+  adjust_plot(ax=ax)
 
   plt.show()
+
+  if savefig:
+    fig.savefig('./images/simulation.pdf', bbox_inches='tight', dpi=1200)
+
+
+def adjust_plot (ax):
+
+  ax.set_axisbelow(True)
+  ax.grid(linestyle='dotted')
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+
+  for tx in ax.xaxis.get_major_ticks():
+    tx.label.set_fontsize(12)
+
+  for ty in ax.yaxis.get_major_ticks():
+    ty.label.set_fontsize(12)
